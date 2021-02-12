837e0f9d324c23971a4d1f2cc3c38aad076a422e
Nuno Pássaro <nuno.passaro@25friday.com>
2021-02-12 11:31:43 +0000

    :sparkles: add markdown render to day description

--- app/javascript/packs/add_surf_day.jsx
+++ app/javascript/packs/add_surf_day.jsx
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from "react"
-import ReactDOM from "react-dom"
-import PropTypes from "prop-types"
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import ReactMarkdown from 'react-markdown'
 
 export function AddSurfDay(props) {
   const [dayAwesomenessRating, setDayAwesomenessRating ] = useState("so-so");
+  const [dayDescription, setDayDescription ] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+
 
   const handleDayAwesomenessRatingChange = ({ target }) => {
     setDayAwesomenessRating(target.value);
   }
 
-  const [dayDescription, setDayDescription ] = useState('');
-
-  useEffect(() => {
-    console.log("Description:", dayDescription);
-  });
+  const handleStartEditingDayDescription = ({ target }) => {
+    setIsEditing(true);
+  }
 
   const handleDayDescriptionChange = ({ target }) => {
     setDayDescription(target.value);
   }
 
-  const handleSubmit = ({ target }) => {
-    console.console.warn("Submitted", target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // This should be on a effect after successful submission
+    setIsEditing(false);
   }
 
   return (
@@ -29,13 +33,38 @@ export function AddSurfDay(props) {
         Was it good?
         <input type="radio" value="1" value={dayAwesomenessRating} onChange={handleDayAwesomenessRatingChange} />
       </label>
-      <label>
-        Tell me more:
-        <textarea value={dayDescription} onChange={handleDayDescriptionChange} placeholder="Hey! How was it grom?">
-        </textarea>
-      </label>
+      <DayDescription 
+        dayDescription={dayDescription}
+        isEditing={isEditing}
+        onDayDescriptionChange={handleDayDescriptionChange}
+        onClickDayDescription={handleStartEditingDayDescription}
+      />
       <input type="submit" value="Submit" />
     </form>
+  );
+}
+
+function DayDescription(props) {
+  if(props.isEditing) {
+   return(
+    <label>
+    Tell me more:
+    <textarea 
+      className="DayDescription-show" 
+      value={props.dayDescription} 
+      onChange={props.onDayDescriptionChange} 
+      placeholder="Hey! How was it grom?">
+    </textarea>
+  </label> 
+    );
+  }
+  return (
+    <div 
+      className="DayDescription-show"
+      onClick={props.onClickDayDescription}
+    >
+      <ReactMarkdown children={props.dayDescription} />
+    </div>
   );
 }
 
