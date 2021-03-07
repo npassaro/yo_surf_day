897e59f0604a7b75f9869b880ba6c7727a235fa0
Nuno Pássaro <nuno.passaro@25friday.com>
2021-03-07 16:49:41 +0000

    :sparkles: add day date and time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,9 @@
+# https://stackoverflow.com/questions/7490660/converting-wind-direction-in-angles-to-text-words
+# def degToCompass(num):
+#   val=int((num/22.5)+.5)
+#   arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+#   print arr[(val % 16)]
+
 class HomeController < ApplicationController
   def index
   end
--- app/javascript/packs/add_surf_day.jsx
+++ app/javascript/packs/add_surf_day.jsx
@@ -2,56 +2,94 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import { DayDescription } from "./day_description";
+import { DayRating } from "./day_rating";
+import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
+import locale from "date-fns/locale/en-GB";
+
+registerLocale("en-GB", locale);
+setDefaultLocale("en-GB");
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export function AddSurfDay(props) {
-  const [dayAwesomenessRating, setDayAwesomenessRating ] = useState("so-so");
-  const [dayDescription, setDayDescription ] = useState('');
-  const [isEditing, setIsEditing] = useState(false);
+    const awesomenessDayRatings = {
+        "Nah": {
+            value: 1,
+            text: "Nah",
+        }, 
+        "SoSo": {
+            text: "So-So",
+            value: 2
+        }, 
+        "Brah": {
+            text: "Brahhh!",
+            value: 3
+        }
+      };
+    const [dayAwesomenessRating, setDayAwesomenessRating ] = useState(awesomenessDayRatings["SoSo"].value);
+    const [dayDateAndTime, setDayDateAndTime ] = useState(new Date());
+    const [dayDescription, setDayDescription ] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
 
+    const handleDayAwesomenessRatingChange = (value) => {
+        setDayAwesomenessRating(value);
+    };
 
-  const handleDayAwesomenessRatingChange = ({ target }) => {
-    setDayAwesomenessRating(target.value);
-  };
+    const handleStartEditingDayDescription = ({ target }) => {
+        setIsEditing(true);
+    };
 
-  const handleStartEditingDayDescription = ({ target }) => {
-    setIsEditing(true);
-  };
+    const handleDayDescriptionChange = ({ target }) => {
+        setDayDescription(target.value);
+    };
 
-  const handleDayDescriptionChange = ({ target }) => {
-    setDayDescription(target.value);
-  };
+    const handleDayDateAndTimeChange = (dateAndTime) => {
+        setDayDateAndTime(dateAndTime);
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // This should be on a effect after successful submission
-    setIsEditing(false);
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // This should be on a effect after successful submission
+        setIsEditing(false);
+        console.log("Submitting...", event.target.value);
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className="AddSurfDay_form">
-      <label>
-        Was it good?
-        <input
-          type="radio"
-          value={dayAwesomenessRating}
-          onChange={handleDayAwesomenessRatingChange}
-        />
-      </label>
-      <DayDescription 
-        dayDescription={dayDescription}
-        isEditing={isEditing}
-        onDayDescriptionChange={handleDayDescriptionChange}
-        onClickDayDescription={handleStartEditingDayDescription}
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  );
+    return (
+        <div className="DayDescription">
+          <form onSubmit={handleSubmit} className="AddSurfDay_form">
+            <label>
+              Was it good?
+              <DayRating
+                rating={dayAwesomenessRating}
+                onChange={handleDayAwesomenessRatingChange}
+                possibleRatings={awesomenessDayRatings}
+              />
+            </label>
+            <label>
+              <DatePicker
+                showTimeSelect
+                selected={dayDateAndTime}
+                onChange={handleDayDateAndTimeChange}
+                dateFormat="PPpp"
+              />
+            </label>
+            <DayDescription 
+              dayDescription={dayDescription}
+              isEditing={isEditing}
+              onDayDescriptionChange={handleDayDescriptionChange}
+              onClickDayDescription={handleStartEditingDayDescription}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+          <div>Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </div>
+    );
 }
 
 
 AddSurfDay.defaultProps = { };
 
 AddSurfDay.propTypes = {
-  dayDescription: PropTypes.string,
-  dayAwesomeness: PropTypes.number
+    dayDescription: PropTypes.string,
+    dayAwesomeness: PropTypes.number
 };
